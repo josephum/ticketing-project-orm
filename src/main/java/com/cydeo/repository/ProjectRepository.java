a137d1140fd2a9d9da3262a58084e998b78a8c95
Jose <josephum.garcia@gmail.com>
2022-05-10 21:15:03 -0400

    Project controller

---
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Project findByProjectCode(String code);
+
+//    @Transactional
+//    Project save(Project project);
+
+}
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,8 +1,65 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProjectServiceImpl implements ProjectService {
+
+    final private ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    @Override
+    public ProjectDTO getByProjectCode(String code) {
+        return projectMapper.convertToDto(projectRepository.findByProjectCode(code));
+    }
+
+    @Override
+    public List<ProjectDTO> listAllProjects() {
+        return projectRepository.findAll().stream().map(projectMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(ProjectDTO dto) {
+        dto.setProjectStatus(Status.OPEN);
+        projectRepository.save(projectMapper.convertToEntity(dto));
+    }
+
+    @Override
+    public void update(ProjectDTO dto) {
+        Project project = projectRepository.findByProjectCode(dto.getProjectCode());
+        Project convertedProject = projectMapper.convertToEntity(dto);
+        convertedProject.setId(project.getId());
+        // Get status from DB too!!!
+        convertedProject.setProjectStatus(project.getProjectStatus());
+        projectRepository.save(convertedProject);
+    }
+
+    @Override
+    public void delete(String code) {
+        Project project = projectRepository.findByProjectCode(code);
+        project.setIsDeleted(true);
+        projectRepository.save(project);
+    }
+
+    @Override
+    public void complete(String code) {
+        Project project = projectRepository.findByProjectCode(code);
+        project.setProjectStatus(Status.COMPLETE);
+        projectRepository.save(project);
+    }
 }
